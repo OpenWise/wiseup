8b77ad3b52ebea82b401e85d0478e3426536dbd8
Yevgeniy Kiveisha <lodmialk@gmail.com>
2015-02-15 00:18:40 +0200

    cordova-app: added new switch module (initial work)
    Signed-off-by: Yevgeniy Kiveisha <lodmialk@gmail.com>

--- src/cordova/www/js/directives/sensor.js
+++ src/cordova/www/js/directives/sensor.js
@@ -26,7 +26,7 @@ angular.module('app').directive('sensor', function($http) {
                         alert ("MOVEMENT_TYPE");
                     break;
                     case SWITCH_TYPE:
-                        SendCommand ($http, element);
+                        alert ("SWITCH_TYPE");
                     break;
                 }
             }
@@ -38,6 +38,49 @@ angular.module('app').directive('sensor', function($http) {
         var currentValue = parseInt(angular.element(element[0].querySelector('.sensor-value')).html());
 
         $http.get('http://10.0.0.15/api/sensors/' + currentName + '/' + (currentValue+1)%2).
+            success(function(data, status, headers, config) {
+            }).
+            error(function(data, status, headers, config) {
+        });
+    }
+});
+
+angular.module('app').directive('toggleButton', function($http) {
+	return {
+		require: 'ngModel',
+		scope: {
+			sensorId: '@sensorId',
+			activeText: '@activeText',
+			inactiveText: '@inactiveText',
+			sensorState: '=ngModel'
+		},
+		replace: true,
+		transclude: true,
+		template: '<div>' +
+			'<button class="pure-button" ng-class="{\'pure-button-primary\': state.value}" ng-click="state.toggle()">{{activeText}}</button>' +
+			'<button class="pure-button" ng-class="{\'pure-button-primary\': !state.value}" ng-click="state.toggle()">{{inactiveText}}</button>' +
+			'</div>',
+		link: function postLink(scope) {
+			scope.sensorState = "BLACK";
+			scope.state = {
+				value: false,
+				color: "BLACK",
+				toggle: function() {
+					this.value = !this.value;
+					if (this.color == "BLACK") {
+						this.color = "RED";
+					} else {
+						this.color = "BLACK";
+					}
+					scope.sensorState = this.color;
+					SendCommand ($http, scope.sensorId, (this.value == true) ? 1 : 0);
+				}
+			};
+		}
+	}
+	
+	function SendCommand ($http, sensorId, action) {
+        $http.get('http://10.0.0.15/api/sensors/' + sensorId + '/' + action).
             success(function(data, status, headers, config) {
             }).
             error(function(data, status, headers, config) {
